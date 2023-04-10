aadace22c06537ac933f1949b5e88d1d55fd14a8
Andre Poli <andre.lpoli@hotmail.com>
2023-04-10 18:38:00 -0300

    Excluindo imagens HomePage

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -5,15 +5,6 @@ import mortyHomeImg from "../images/Morty-home.webp"
 import jerryHomeImg from "../images/jerry-home.jpeg"
 import bethHomeImg from "../images/beth-home.webp"
 import summerHomeImg from "../images/summer-home.webp"
-import galleryImg1 from "../images/cover-pic.jpg"
-import galleryImg2 from "../images/cover-pic-2.jpg"
-import galleryImg3 from "../images/cover-pic-3.jpg"
-import galleryImg4 from "../images/r&mHome.png"
-import galleryImg5 from "../images/gallery-img.jpg"
-import galleryImg6 from "../images/gallery-img-2.jpg"
-import galleryImg7 from "../images/gallery-img-3.png"
-import galleryImg8 from "../images/gallery-img-4.png"
-
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 const HomePage = () => {
@@ -55,29 +46,12 @@ return (
         <p>Are you new to Rick and Morty? No worries! We're here to introduce you to this fantastic animated sitcom. Created by Justin Roiland and Dan Harmon, Rick and Morty takes you on a thrilling adventure through space and alternate dimensions, as you follow the misadventures of mad scientist Rick Sanchez and his good-hearted, yet easily influenced, grandson Morty Smith.</p>
         <p>Combining humor, science fiction, and deep philosophical themes, Rick and Morty has captivated audiences worldwide and garnered a passionate fan-base. So buckle up and get ready to explore the many bizarre and hilarious worlds of Rick and Morty!</p>
 
-        </div>
-
-        <h2> Embark on a Visual Adventure: Rick and Morty Gallery Awaits! </h2>
-
-        <div className="gallery-home"> 
-
-       
-       <img src = {galleryImg1} alt = "rick-and-morty-epsode" />
-       <img src = {galleryImg2} alt = "rick-and-morty-epsode" />
-       <img src = {galleryImg3} alt = "rick-and-morty-epsode" />
-       <img src = {galleryImg4} alt = "rick-and-morty-epsode" />
-       <img src = {galleryImg5} alt = "rick-and-morty-epsode" />
-       <img src = {galleryImg6} alt = "rick-and-morty-epsode" />
-       <img src = {galleryImg7} alt = "rick-and-morty-epsode" />
-       <img src = {galleryImg8} alt = "rick-and-morty-epsode" />
-
-        </div>
+        </div>       
 
         <button className="back-top-btn"> <a href="#home-nav-bar">Back to the Top</a>  </button>
 </div>
 
 )
-
 
 }
 
